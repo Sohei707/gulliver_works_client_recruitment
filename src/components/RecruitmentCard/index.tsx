9ca494f0a4c2bc741c15905a1ff1db168cf04802
Sohei707 <sohei.15157.gtgt@gmail.com>
2021-05-22 17:21:57 +0900

    RecruitmentCardの実装

--- src/components/RecruitmentCard/index.tsx
+++ src/components/RecruitmentCard/index.tsx
@@ -2,7 +2,17 @@ import React from "react";
 import styles from "./style.module.scss";
 
 const RecruitmentCard = () => {
-  return <div className={styles.card}>recruitmentCard</div>;
+  return (
+    <div className={styles.card}>
+      <div className={styles.image}></div>
+      <div className={styles.box}>
+        <div className={styles.recruitmentName}>
+          3度の飯よりReact！フロントエンドのエキスパートになりたいエンジニア募集！
+        </div>
+        <div className={styles.companyName}>株式会社SIMULA</div>
+      </div>
+    </div>
+  );
 };
 
 export default RecruitmentCard;
