c677124550b5079e2a19ee2f71a7f5a972a98308
Sohei707 <sohei.15157.gtgt@gmail.com>
2021-06-18 13:31:28 +0900

    募集名変更

--- src/components/RecruitmentCard/index.tsx
+++ src/components/RecruitmentCard/index.tsx
@@ -6,9 +6,7 @@ const RecruitmentCard = () => {
     <div className={styles.card}>
       <div className={styles.image}></div>
       <div className={styles.box}>
-        <div className={styles.recruitmentName}>
-          3度の飯よりReact！フロントエンドのエキスパートになりたいエンジニア募集！
-        </div>
+        <div className={styles.recruitmentName}>募集名の変更</div>
         <div className={styles.companyName}>株式会社SIMULA</div>
       </div>
     </div>
